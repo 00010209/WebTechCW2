b18c899d2298aca7a4d90b11bcea628dbac59279
00010209 <Rainy>
2021-04-03 14:01:24 +0500

    created layout and home page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express =require('express')
 const app = express()
 
+
+app.set('view engine', 'pug')
+
 app.get('/', (req,res) => {
-    res.send('Hello')
+    res.render('home')
 })
 
 app.listen(8000, err => {
