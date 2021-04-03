0763fd12506fd7e6425a9e30650132fbea4f9ad4
00010209 <Rainy>
2021-04-03 21:23:23 +0500

    Create operations have been done

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-const express =require('express')
+const express = require('express')
 const app = express()
+
+const fs = require('fs')
+
 
 
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
 
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/create', (req,res) => {
+app.get('/create', (req, res) => {
     res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const author = req.body.author
+    const description = req.body.description
+    
+    if (title.trim() === '' && description.trim() === '' && author.trim() === ''){
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/notes.json', (err, data) => {
+            if (err) throw err 
+
+            const notes = JSON.parse(data)
+
+            notes.push({
+                id: id (),
+                title: title,
+                author: author,
+                description: description
+            })
+
+
+            fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+                if (err) throw err 
+
+                res.render('create', { success: true} )
+            })
+        })
+    }
+
 })
 
 const notes = ['Some Title', 'Some Title 2']
 
-app.get('/notes', (req,res) =>{
+app.get('/notes', (req, res) =>{
     res.render('notes', {notes: notes})
 })
 
@@ -30,3 +65,8 @@ app.listen(8000, err => {
 
     console.log('Server is running on port 8000')
 })
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
